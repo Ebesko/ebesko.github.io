5352d6d86d1117e1676f5484b7c8e464df610ce5
Ebesko <118739260+Ebesko@users.noreply.github.com>
2024-05-25 23:54:35 +0200

    'It' helped does it work?

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-﻿function showStuff(id_to_show, link_block, clicked_link) {
+﻿function showStuff1(id_to_show, link_block, clicked_link) {
     document.getElementById(id_to_show).style.display = 'block';
     // hide the lorem ipsum text
     document.getElementById(link_block).style.display = 'block';
     // hide the link
     clicked_link.style.display = 'block';
+}
+
+function showStuff(id, text, btn) {
+    document.getElementById(id).style.display = 'block';
+    btn.style.display = 'none';
 }
