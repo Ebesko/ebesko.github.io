19f10fd9a9383ddefb6389d21f755e1c4325ce40
Ebesko <118739260+Ebesko@users.noreply.github.com>
2024-05-27 14:22:04 +0200

    ++

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
-﻿function showStuff1(id_to_show, link_block, clicked_link) {
-    document.getElementById(id_to_show).style.display = 'block';
-    // hide the lorem ipsum text
-    document.getElementById(link_block).style.display = 'block';
-    // hide the link
-    clicked_link.style.display = 'block';
-}
-
-function showStuff(id, text, btn) {
-    document.getElementById(id).style.display = 'block';
-    document.getElementById(text).style.display = 'block';
+﻿function showStuff(id, text, btn) {
+    document.getElementById(id).style.display = 'inline';
+    document.getElementById(text).style.display = 'inline';
     btn.style.display = 'block';
-}
-
-
-const data = new FormData(myform);
-function megatest(data) {
-    console.log("total" + data);//NON
-    var tonk = Object.fromEntries(data);
-    console.log("total2" + data.value);//OUI
-    var nom = document.getElementById("semestre");
-    console.log(nom);//NON
-    console.log(nom.value);//OUI
 }
 
 function getdiffvalues() {
@@ -35,9 +16,3 @@ function getdiffvalues() {
     console.log("année: " + year.value + "nbftyp: " + nbftyp2.value + "semestre: " + semester.value);//-------------------------------------------------------OUI
 }
 
-function handleSubmit(e) {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const formProps = Object.fromEntries(formData);
-    console.log(formProps);
-}
