ef70c309e202dd1238bd7b8e289b8d22c6025731
Ebesko <118739260+Ebesko@users.noreply.github.com>
2024-05-27 15:28:38 +0200

    Tonk
    Nom en honneur de la fameuse et regrettée variable qui servait à rien

--- script.js
+++ script.js
@@ -4,6 +4,12 @@
     btn.style.display = 'block';
 }
 
+function hiddenBefore(id, text, btn) {
+    document.getElementById(id).style.display = 'block';
+    document.getElementById(text).style.display = 'block';
+    btn.style.display = 'block';
+}
+
 function getdiffvalues() {
     const data = new FormData(myform);
     var tonk = Object.fromEntries(data);
@@ -11,10 +17,10 @@ function getdiffvalues() {
     var nbftyp2 = document.getElementById("minortyptest");
     var nbfname = document.getElementById("minor");
     var semester = document.getElementById("semester");
-    console.log("Année d'inscription: " + year.value + ", type de mineure: " + nbftyp2.value + ", nom de la mineure: " + nbfname.value + ", semestre: " + semester.value);
+    console.log("Année d'inscription: 20" + year.value + ", type de mineure: " + nbftyp2.value + ", nom de la mineure: " + nbfname.value + ", semestre: " + semester.value);
 }
 
 function minitest() {
     var nbfname = document.getElementById("minor");
-    document.getElementById("to_show_when_generate").innerHTML = nbfname;
+    document.getElementById("to_show_when_generate").innerHTML = nbfname.value;
 }
