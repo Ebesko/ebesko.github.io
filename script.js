7eefbb8edf39a995ef7259d26698b7c9174d7fb6
Ebesko <118739260+Ebesko@users.noreply.github.com>
2024-05-27 15:07:38 +0200

    minitest

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 ﻿function showStuff(id, text, btn) {
-    document.getElementById(id).style.display = 'inline';
-    document.getElementById(text).style.display = 'inline';
+    document.getElementById(id).style.display = 'block';
+    document.getElementById(text).style.display = 'none';
     btn.style.display = 'block';
 }
 
@@ -8,11 +8,13 @@ function getdiffvalues() {
     const data = new FormData(myform);
     var tonk = Object.fromEntries(data);
     var year = document.getElementById("year");
-    //var nbftyp = document.getElementById("minortyp");
     var nbftyp2 = document.getElementById("minortyptest");
+    var nbfname = document.getElementById("minor");
     var semester = document.getElementById("semester");
-    console.log("TONK: " + tonk.value);
-    console.log("total qui marche: " + data.value);//---------------------------------------------------------------------------------------------------------OUI
-    console.log("année: " + year.value + "nbftyp: " + nbftyp2.value + "semestre: " + semester.value);//-------------------------------------------------------OUI
+    console.log("Année d'inscription: " + year.value + ", type de mineure: " + nbftyp2.value + ", nom de la mineure: " + nbfname.value + ", semestre: " + semester.value);
 }
 
+function minitest() {
+    var nbfname = document.getElementById("minor");
+    document.getElementById("to_show_when_generate").innerHTML = nbfname;
+}
